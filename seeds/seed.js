04d627116a51e956589e838f42befa42b5c7754f
berekt24 <bashenafi35@gmail.com>
2022-12-09 19:27:20 -0500

    seeds and comments changed

---
+++ seeds/seed.js
@@ -0,0 +1,24 @@
+const sequelize = require('../config/connection');
+const { User, BlogPost, BlogComment } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const posts = await BlogPost.bulkCreate(postData, {
+    individualHooks: true,
+    returning: true,
+  });
+ 
+
+  process.exit(0);
+};
+
+seedDatabase();
